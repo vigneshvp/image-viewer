313859079416cbe0d667f5403550050a67b3dfbd
vigneshvp <viki3110@gmail.com>
2021-04-12 10:24:11 +0530

    Added Login Card with Username, Password and Button

--- src/screens/login/Login.js
+++ src/screens/login/Login.js
@@ -1,15 +1,43 @@
 import React, {Component} from "react";
 import './Login.css'
 import Header from "../../common/header/Header";
+import {Button, Card, FormControl, Input, InputLabel} from "@material-ui/core";
 
-class Login extends Component {
+export default class Login extends Component {
+    constructor() {
+        super();
+        this.state = {
+            username: "",
+            password: "",
+        };
+    }
+
     render() {
         return (
             <div>
                 <Header/>
+                <Card>
+                    <h2>LOGIN</h2>
+                    <FormControl fullWidth={true} margin="normal">
+                        <InputLabel htmlFor="username">Username *</InputLabel>
+                        <Input
+                            id="username"
+                        />
+                    </FormControl>
+                    <FormControl fullWidth={true} margin="normal">
+                        <InputLabel htmlFor="password">Password *</InputLabel>
+                        <Input
+                            id="password"
+                            type="password"
+                        />
+                    </FormControl>
+                    <Button
+                        variant="contained"
+                        color="primary">
+                        LOGIN
+                    </Button>
+                </Card>
             </div>
-        )
+        );
     }
 }
-
-export default Login;
